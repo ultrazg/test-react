2c8ee8bc160d0763e3007e3cd116458793bbe849
unknown <742005790@qq.com>
2021-09-16 10:21:14 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import CountCom from './components/Hook';
 import StarList from './components/starList';
 import EchartsDemo from "./views/EchartsDemo";
 import ModalDemo from './views/ModalDemo';
+import WordFun from './components/wordFun';
 
 function App() {
   return (
     <div>
       <h3>hello world</h3>
-      <CountCom/>
+      {/* <CountCom/>
       <StarList name='手抓饼' score={3}/>
       <StarList name='苹果汁' score={2}/>
-      <StarList name='烤鱿鱼' score={4}/>
+      <StarList name='烤鱿鱼' score={4}/> */}
       {/*<EchartsDemo/>*/}
-      <ModalDemo/>
+      {/* <ModalDemo/> */}
+      <WordFun />
     </div>
   );
 }
